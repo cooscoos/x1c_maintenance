651f40bd066adf578050efad3d476242ac8c9225
cooscoos <alias@domain.com>
2025-01-17 08:59:36 +0000

    first

---
+++ scripts/generate_readme.py
@@ -0,0 +1,44 @@
+"""Script to generate README file from a maintenance.csv"""
+
+import pandas as pd
+from pathlib import Path
+
+# File paths
+CSV_FILE = Path.cwd() / 'maintenance.csv'
+README_FILE = Path.cwd() / 'README.md'
+HEADER_FILE = Path.cwd() / 'scripts' / 'header.md'
+
+
+def csv_to_markdown(csv_path: Path) -> str:
+    """Converts a csv to a markdown table using pandas."""
+    df = pd.read_csv(csv_path)
+
+    # Create a new column with markdown-formatted links
+    df['Maintenance'] = df.apply(
+        lambda row: f"[{row['Maintenance']}]({row['Link']})"
+            if pd.notna(row['Link']) else row['Maintenance'],
+            axis=1
+    )
+
+    # Link column not needed in markdown table
+    df = df.drop(columns=['Link'])
+
+    # Convert DataFrame to Markdown table
+    markdown = df.to_markdown(index=False)
+
+    return markdown
+
+
+def main():
+    """Generate README.md from csv file."""
+    # Read header text from file
+    with open(HEADER_FILE, 'r', encoding='utf-8') as f:
+        header_text = f.read()
+
+    markdown_table = csv_to_markdown(CSV_FILE)
+    with open(README_FILE, 'w', encoding='utf-8') as f:
+        f.write(header_text + "\n\n" + markdown_table)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/header.md
@@ -0,0 +1,6 @@
+# Maintenance checklist for X1/X1C printer
+
+Maintenance checklist for an X1 or X1C 3D printer. The table lists how often maintenance actions should be done when printing with non-ABS or ABS filaments.
+
+- The table below lists is sorted by column `Priority (non-ABS)`.
+- A `Priority (ABS)` column is provided to sort maintenance actions by how frequently they should be done for ABS printing.
